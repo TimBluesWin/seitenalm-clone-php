760a0a89a64fbb54749c8293931a94927702d9da
Timmy <timisutantyo@gmail.com>
2024-08-16 08:22:07 +0200

    Type of commit: refactor
    change view file name to "form" instead of the "welcome" default name.

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -25,7 +25,7 @@ class RequestController extends Controller
         $yearsList = ['2024', '2023', '2022', '2021', '2020', '2019', '2018', '2017', '2016', '2015', 
         '2014', '2013', '2012', '2011', '2010', '2009', '2008', '2007', '2006'];
         // pass the list of countries into the view.
-        return view('welcome', [
+        return view('form', [
             'countries' => $countries,
             'daysList' => $daysList,
             'monthsList' => $monthsList,
